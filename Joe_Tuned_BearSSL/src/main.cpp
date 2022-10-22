f993c13be20c11c64790999c81c239d475342263
Joseph Brendler <joseph.brendler@gmail.com>
2022-10-22 18:46:52 -0400

    bugfix

--- Joe_Tuned_BearSSL/src/main.cpp
+++ Joe_Tuned_BearSSL/src/main.cpp
@@ -17,21 +17,12 @@ const char *path = myPATH;
 const char *SSL_host = mySSL_HOST;
 const int SSL_port = mySSL_PORT;
 
-// use these for loop-trace timing
-uint32_t now = 0;
-uint32_t delta = 0, delta2 = 0, delta3 = 0;
-
 // Function dummy declarations (so I can move them below loop())
 void setClock();
 void separator(String msg);
 void fetchURL(BearSSL::WiFiClientSecure *client, const char *host, const uint16_t port, const char *path);
 
-/* Connect using a known key, CA certificate, and a custome cipher list --
-** Known key:  The server certificate can be completely ignored, with its public key
-hardcoded in the application. This should be secure as the public key
-needs to be paired with the private key of the site, which is obviously
-private and not shared.  A MITM without the private key would not be
-able to establish communications.
+/* Connect using a CA certificate and a custome cipher list --
 ** CA Certificate:  A specific certification authority can be passed in and used to validate
 a chain of certificates from a given server.  These will be validated
 using BearSSL's rules, which do NOT include certificate revocation lists.
@@ -41,21 +32,36 @@ BearSSL does verify the notValidBefore/After fields.
 ** The ciphers used to set up the SSL connection can be configured to
 only support faster but less secure ciphers  -- If you care more about security
 you won't want to do this, but if you need to maximize battery life, these
-may make sense.
-*/
-void fetchKnownKeyCaCertCustomCipherList()
+may make sense. */
+void fetchCaCertCustomCipherList()
 {
-  separator("fetchKnownKeyCaCertCustomCipherList()");
+  separator("fetchCaCertCustomCipherList()");
   BearSSL::WiFiClientSecure client;
-  BearSSL::PublicKey key(myPUBKEY);
-  client.setKnownKey(&key);
   BearSSL::X509List cert(myCERT);
   client.setTrustAnchors(&cert); // Note: this method needs time to be set
   client.setCiphers(myCustomCipherList);
-  now = millis();
   fetchURL(&client, SSL_host, SSL_port, path);
-  delta = millis() - now;
-  Serial.printf("  Time: %dms\n\n", delta);
+}
+
+/* Connect using a known key and a custome cipher list --
+** Known key:  The server certificate can be completely ignored, with its public key
+hardcoded in the application. This should be secure as the public key
+needs to be paired with the private key of the site, which is obviously
+private and not shared.  A MITM without the private key would not be
+able to establish communications.
+** The ciphers used to set up the SSL connection can be configured to
+only support faster but less secure ciphers  -- If you care more about security
+you won't want to do this, but if you need to maximize battery life, these
+may make sense. */
+void fetchKnownKeyCustomCipherList()
+{
+  separator("fetchKnownKeyCustomCipherList()");
+
+  BearSSL::WiFiClientSecure client;
+  BearSSL::PublicKey key(myPUBKEY);
+  client.setKnownKey(&key);
+  client.setCiphers(myCustomCipherList);
+  fetchURL(&client, SSL_host, SSL_port, path);
 }
 
 void setup()
@@ -77,8 +83,10 @@ void setup()
 
   delay(500);
   setClock();
-
-  fetchKnownKeyCaCertCustomCipherList();
+  delay(500);
+  fetchKnownKeyCustomCipherList();
+  delay(500);
+  fetchCaCertCustomCipherList();
 }
 
 void loop()
@@ -89,13 +97,14 @@ void loop()
 // Set time via NTP, as required for x.509 validation
 void setClock()
 {
-  configTime(3 * 3600, 0, "pool.ntp.org", "time.nist.gov");
+  configTime(int(3 * 60 * 60), 0, "time.nist.gov", "pool.ntp.org");
+  // configTime(int(3 * 60 * 60), 0, "oromis.brendler");
 
   Serial.print("Waiting for NTP time sync: ");
   time_t now = time(nullptr);
   while (now < 8 * 3600 * 2)
   {
-    delay(500);
+    delay(4010); // systems that query for time more frequently than every 4 sec will be refused service
     Serial.print(".");
     now = time(nullptr);
   }
